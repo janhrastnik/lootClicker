d1a9905c0d54981f7fce03245078789c3f8da270
jan hrastnik <jan.hrastnik2@gmail.com>
2019-04-08 23:19:04 +0200

    fixed bloc

--- lib/clickerbloc.dart
+++ lib/clickerbloc.dart
@@ -1,9 +1,43 @@
-import 'main.dart';
-import 'package:bloc/bloc.dart';
 import 'dart:math';
+import 'package:bloc/bloc.dart';
+import 'main.dart';
+
+class DungeonBloc extends Bloc<List<DungeonTile>, List<DungeonTile>> {
+  @override
+  List<DungeonTile> get initialState => [
+    DungeonTile(event: DungeonEvent(eventType: "loot", length: 10)),
+    DungeonTile(event: DungeonEvent(eventType: "fight", length: 10)),
+    DungeonTile(event: DungeonEvent(eventType: "puzzle", length: 10))
+  ];
+
+  @override
+  Stream<List<DungeonTile>> mapEventToState(List<DungeonTile> event) async* {
+    int r = Random().nextInt(eventTypes.length);
+    if (event[1].event.eventType == "loot") {
+
+    }
+    switch (event.length) {
+      case 3:
+        final List<DungeonTile> newList = List.from(event, growable: true);
+        newList.add(DungeonTile(
+            event: DungeonEvent(eventType: eventTypes[r], length: 10)));
+        yield newList;
+        break;
+      case 4:
+        final List<DungeonTile> newList = List.from(event);
+        newList.removeAt(0);
+        yield newList;
+        break;
+    }
+  }
+}
 
 class ClickerBloc extends Bloc<DungeonEvent, double> {
   double get initialState => 0.0;
+  final GoldBloc goldBloc;
+
+  ClickerBloc({this.goldBloc}) : assert(goldBloc != null);
+
 
   Stream<double> mapEventToState(DungeonEvent event) async* {
     print(event.eventType);
@@ -19,6 +53,7 @@ class ClickerBloc extends Bloc<DungeonEvent, double> {
       case "loot":
         event.progress++;
         if (event.progress == event.length) {
+          goldBloc.dispatch(heroGold);
           yield -1;
         } else {
           yield event.progress / event.length;
@@ -36,30 +71,13 @@ class ClickerBloc extends Bloc<DungeonEvent, double> {
   }
 }
 
-class DungeonBloc extends Bloc<List<DungeonTile>, List<DungeonTile>> {
-  int r = Random().nextInt(eventTypes.length);
+class GoldBloc extends Bloc<int, int> {
+  int get initialState => 0;
 
-  @override
-  List<DungeonTile> get initialState => [
-    DungeonTile(event: DungeonEvent(eventType: "loot", length: 10)),
-    DungeonTile(event: DungeonEvent(eventType: "fight", length: 10)),
-    DungeonTile(event: DungeonEvent(eventType: "puzzle", length: 10))
-  ];
-
-  @override
-  Stream<List<DungeonTile>> mapEventToState(List<DungeonTile> event) async* {
-    switch(event.length) {
-      case 3:
-        final List newList = List.from(event, growable: true);
-        newList.add(DungeonTile(event: DungeonEvent(eventType: eventTypes[r], length: 10)));
-        yield newList;
-        break;
-      case 4:
-        final List newList = List.from(event);
-        newList.removeAt(0);
-        yield newList;
-        break;
-    }
+  Stream<int> mapEventToState(int gold) async* {
+    int newGold = gold;
+    print("newGold is " + newGold.toString());
+    newGold++;
+    yield newGold;
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'clickerbloc.dart';
+import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'clickerbloc.dart';
 
-double TILE_LENGTH;
 List eventTypes = ["loot", "fight", "puzzle"];
+double TILE_LENGTH;
+int heroGold = 0;
 
-void main() => runApp(MyApp());
+class MyBlocDelegate extends BlocDelegate {
+  @override
+  void onError(Object error, StackTrace stacktrace) {
+    super.onError(error, stacktrace);
+    print(error);
+  }
 
-class MyApp extends StatelessWidget {
-  final ClickerBloc _clickerBloc = ClickerBloc();
-  final DungeonBloc _dungeonBloc = DungeonBloc();
+  @override
+  void onTransition(Transition transition) {
+    super.onTransition(transition);
+    print(transition);
+  }
+}
+
+void main() {
+  BlocSupervisor().delegate = MyBlocDelegate();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  DungeonBloc _dungeonBloc;
+  ClickerBloc _clickerBloc;
+  GoldBloc _goldBloc;
+
+  @override
+  void initState() {
+    _goldBloc = GoldBloc();
+    _dungeonBloc = DungeonBloc();
+    _clickerBloc = ClickerBloc(goldBloc: _goldBloc);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +54,13 @@ class MyApp extends StatelessWidget {
       ),
       home: BlocProviderTree(
         blocProviders: <BlocProvider>[
-          BlocProvider<ClickerBloc>(bloc: _clickerBloc),
           BlocProvider<DungeonBloc>(bloc: _dungeonBloc),
+          BlocProvider<ClickerBloc>(bloc: _clickerBloc),
+          BlocProvider<GoldBloc>(bloc: _goldBloc)
         ],
         child: DungeonList(),
       ),
-      );
+    );
   }
 }
 
@@ -44,21 +77,20 @@ class DungeonListState extends State<DungeonList> {
     DungeonTile(event: DungeonEvent(eventType: "puzzle", length: 10))
   ];
 
-  _scrollToMiddle() {
+  void _scrollToMiddle() {
     _scrollController.jumpTo(MediaQuery.of(context).size.width/4);
-    print("OFFSET IS ${_scrollController.offset}");
   }
 
-  _scrollToNextRoom(bloc) {
+  void _scrollDungeon(DungeonBloc bloc) {
     _scrollToMiddle();
-    print(_scrollController.offset);
     bloc.dispatch(_dungeonTiles);
     _scrollController.animateTo(
-      90.0 + MediaQuery.of(context).size.width/2,
-      duration: Duration(seconds: 1),
-      curve: Curves.ease
+        _scrollController.offset + MediaQuery.of(context).size.width/2,
+        duration: Duration(seconds: 1),
+        curve: Curves.ease
     ).then((data) {
       bloc.dispatch(_dungeonTiles);
+      _scrollToMiddle();
     });
   }
 
@@ -71,67 +103,56 @@ class DungeonListState extends State<DungeonList> {
   @override
   Widget build(BuildContext context) {
     TILE_LENGTH = MediaQuery.of(context).size.width/2;
-    final ClickerBloc _clickerBloc = BlocProvider.of<ClickerBloc>(context);
     final DungeonBloc _dungeonBloc = BlocProvider.of<DungeonBloc>(context);
+    final ClickerBloc _clickerBloc = BlocProvider.of<ClickerBloc>(context);
+    final GoldBloc _goldBloc = BlocProvider.of<GoldBloc>(context);
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: GestureDetector(
-              onTap: () {
-                _clickerBloc.dispatch(_dungeonTiles[1].event);
-              },
-              child: Center(
-                child: Stack(
-                  alignment: Alignment(-0.1, 0.0),
-                  children: <Widget>[
-                    ConstrainedBox(
-                      constraints: BoxConstraints(
-                          maxHeight: 200.0
-                      ),
-                      child: BlocBuilder(
-                          bloc: _dungeonBloc,
-                          builder: (BuildContext context, List<DungeonTile> l) {
-                            print("BLOCBUILDER GETS CALLED");
-                            _dungeonTiles = l;
-                            return ListView.builder(
-                              physics: NeverScrollableScrollPhysics(),
-                              controller: _scrollController,
-                              padding: EdgeInsets.all(0.0),
-                              scrollDirection: Axis.horizontal,
-                              shrinkWrap: true,
-                              itemCount: _dungeonTiles.length,
-                              itemBuilder: (BuildContext, int index) => _dungeonTiles[index],
-                            );
-                          }
-                      ),
-                    ),
-                    Text("Hero")
-                  ],
-                ),
+      body: GestureDetector(
+        onTap: () {
+          _clickerBloc.dispatch(_dungeonTiles[1].event);
+        },
+        child: Column(
+          children: <Widget>[
+            BlocBuilder(
+            bloc: _goldBloc,
+            builder: (BuildContext context, int gold) {
+              heroGold = gold;
+              return Text("Gold: " + gold.toString());
+            }
+            ),
+            Expanded(
+              child: BlocBuilder(
+                bloc: _dungeonBloc,
+                builder: (BuildContext context, List<DungeonTile> l) {
+                  print("BLOCBUILDER GETS CALLED");
+                  _dungeonTiles = l;
+                  return ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    controller: _scrollController,
+                    padding: EdgeInsets.all(0.0),
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: _dungeonTiles.length,
+                    itemBuilder: (BuildContext context, int index) =>
+                    _dungeonTiles[index],
+                  );
+                },
               ),
             ),
-          ),
-          BlocBuilder(
-              bloc: _clickerBloc,
-              builder: (BuildContext context, double progress) {
-                print(progress);
-                if (progress == -1) {
-                  _scrollToNextRoom(_dungeonBloc);
+            BlocBuilder(
+                bloc: _clickerBloc,
+                builder: (BuildContext context, double progress) {
+                  print(progress);
+                  if (progress == -1) {
+                    _scrollDungeon(_dungeonBloc);
+                  }
+                  return LinearProgressIndicator(
+                    value: progress,
+                  );
                 }
-                return LinearProgressIndicator(
-                  value: progress,
-                );
-              }
-          ),
-          Row(
-            children: <Widget>[
-              Text("Hero"),
-              Text("Inventory"),
-              Text("Skills")
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -144,16 +165,23 @@ class DungeonTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: TILE_LENGTH,
-      height: 100.0,
-      decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage("assets/mayclover_meadow_example.png"), fit: BoxFit.cover),
-        border: new Border.all(color: Colors.blueAccent)
-      ),
-      alignment: Alignment(0.7, 0.0),
-      child: Text(event.eventType),
+    return Center(
+      child: Container(
+        width: TILE_LENGTH,
+        height: 100.0,
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage("assets/mayclover_meadow_example.png"), fit: BoxFit.cover),
+            border: new Border.all(color: Colors.blueAccent)
+        ),
+        alignment: Alignment(0.7, 0.0),
+        child: Text(event.eventType),
+      )
     );
+  }
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.debug}) {
+    return 'DungeonTile { event: $event }';
   }
 }
 
@@ -161,5 +189,13 @@ class DungeonEvent {
   String eventType;
   int length;
   int progress;
-  DungeonEvent({@required this.eventType, @required this.length, this.progress=0});
+  DungeonEvent({
+    @required this.eventType,
+    @required this.length,
+    this.progress = 0,
+  });
+
+  @override
+  String toString() =>
+      'DungeonEvent { evenType: $eventType, length: $length, progress: $progress }';
 }
